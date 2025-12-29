777364ebee59c1606c280a58ee61ac032cbd128f
RYLCODE (Gabriel Roque) <gabrielroque152830@gmail.com>
2025-12-29 00:18:57 +0000

    Aulas_fundamentos_e_operadores

---
+++ Cursos /Curso_PYTHON/Aula9.py
@@ -0,0 +1,18 @@
+adicao = 10 + 10 # 20
+print('Adição', adicao)
+subtracao = 10 - 5 # 5
+print('Subtração', subtracao)
+multiplicacao = 10 * 10 # 100
+print('Multiplicação', multiplicacao)
+divisao = 10 / 2 # 5.0 = float e apresenta a parte decimal
+print('Divisão', divisao)
+divisao_inteira = 10 // 2.2 # 4.0 = float e trunca a parte decimal
+print('Divisão Inteira', divisao_inteira)
+exponenciacao = 2 ** 10 # 1024
+print('Exponenciação', exponenciacao)
+modulo = 55 % 2 # resto da divisão = 1
+print('Módulo/Reminder', modulo)
+print(10 % 8 == 0) #10 é divisível por 8?
+print(16 % 8 == 0) #16 é divisível por 8?
+print(10 % 2 == 0) #10 é divisível por 2?
+print(15 % 2 == 0) #15 é divisível por 2?
